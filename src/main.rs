490c921fe9a761039398218924528c72c2b30a15
Jacob Odgård Tørring <jacob.torring@gmail.com>
2018-11-16 16:58:11 +0900

    Adds a prompt for deletion, file count and total file size

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate clap;
 //extern crate corm;
 
-use clap::{Arg, App, AppSettings, SubCommand};
-// use std::process::Command;
+use clap::{Arg, App, AppSettings};
+use std::process::Command;
+use std::io;
+use std::io::Write;
 
 //use corm::download::{ftp_download, http_download};
 //use corm::utils;
@@ -16,48 +18,79 @@ fn main() {
         .about("Safe wrapper for rm written in Rust")
         .setting(AppSettings::ColoredHelp)
         .setting(AppSettings::SubcommandsNegateReqs)
-        .subcommand(SubCommand::with_name("passthrough")
-            .about("Passthrough to rm")
-            .arg(Arg::with_name("passthrough-args")
-                .value_name("PASSTHROUGH")
-                .help("The arguments to be passed through to rm")
-            ))
+
+        // Corm
         .arg(Arg::with_name("input-file")
             .value_name("INPUT")
+            .required(true)
+            .min_values(1)
             .help("Set the input file to delete"))
         .arg(Arg::with_name("config-file")
             .short("c")
             .long("config")
             .value_name("CONFIG")
             .help("Sets a custom config file"))
+            
+        // Passthrough to original rm
+        /*.subcommand(SubCommand::with_name("passthrough")
+            .about("Passthrough to rm")
+            .arg(Arg::with_name("passthrough-args")
+                .value_name("PASSTHROUGH")
+                .help("The arguments to be passed through to rm")
+            ))
+        */
+
         .get_matches();
 
-//    let files: Vec<_> = matches.values_of("input-file").unwrap().collect();
-    if let Some(ref matches) = matches.subcommand_matches("passthrough") {
+    let files: Vec<_> = matches.values_of("input-file").unwrap().collect();
+
+    let borrowed_string: &str = &files[0].to_owned();
+    let mut owned_string2: String = "du -sch ".to_owned();
+    let borrowed_string2: &str = " | grep total$";
+    owned_string2.push_str(borrowed_string);
+    owned_string2.push_str(borrowed_string2);
+    print!("Deleting {} file(s). Total size: ", files.len());
+    print!("{}", sh(&owned_string2));
+
+    print!("Are you sure? [y/N] ");
+    std::io::stdout().flush().unwrap();
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+        //    println!("{}", input);
+        }
+        Err(error) => println!("error: {}", error),
+    }
+
+    match input.trim() {
+        "y" | "Y" | "yes" | "Yes" | "YES" => {
+            let mut owned_string: String = "rm ".to_owned();
+
+            owned_string.push_str(borrowed_string);
+
+            sh(&owned_string);
+        },
+        _ => println!("Canceled execution")
+    }
+
+    /*if let Some(ref matches) = matches.subcommand_matches("passthrough") {
         if matches.is_present("passthrough-args") {
             let files: Vec<_> = matches.values_of("passthrough-args").unwrap().collect();
-            println!("{}", files[0]);
+            println!("Input {}", files[0]);
+            sh(files[0]);
         }
-    }
-    //println!("Deleting this file: {}", files[0]);
-/*
+    }*/
+}
 
-    let output = if cfg!(target_os = "windows") {
-        Command::new("cmd")
-                .args(&["/C", "echo hello"])
-                .output()
-                .expect("failed to execute process")
-    } else {
-
-        let mut owned_string: String = "rm ".to_owned();
-        let another_string: String = files[0].to_owned();
-        owned_string.push_str(&another_string);
-
-        Command::new("sh")
+fn sh(command: &str) -> String {
+    let output = 
+        Command::new("sh") 
                 .arg("-c")
-                .arg(owned_string)
+                .arg(command)
                 .output()
-                .expect("failed to execute process")
-   };
-   println!("{}", String::from_utf8_lossy(&output.stdout));*/
+                .expect("failed to execute process");
+
+    // println!("Shell {}", command);
+    return String::from_utf8_lossy(&output.stdout).replace("\ttotal", " ");
+    // println!("Output {}", String::from_utf8_lossy(&output.stdout).replace("\ttotal", " "));
 }
